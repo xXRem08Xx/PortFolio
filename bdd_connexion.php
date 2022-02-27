c855582efa364e3f0ffdbcd1bee7f571d44f399c
xXRem08Xx <maissa.rem08@gmail.com>
2022-02-27 13:10:28 +0100

    ajout du fichier bdd_connexion.php

---
+++ bdd_connexion.php
@@ -0,0 +1,15 @@
+<?php
+      //Connexion à la base de données
+      $nomBase = "rmaissa_portfolio";
+      $serveur = "172.28.100.3";
+      $utilisateur = "rmaissa";
+      $mdp = "elini01";
+
+      $bdd = mysqli_connect($serveur, $utilisateur, $mdp, $nomBase);
+      
+      //on verifie si la connexion est effectué
+      if(!$bdd)
+      {
+        die('Erreur : '.mysqli_connect_error());
+      }
+      echo 'connexion reussi';
