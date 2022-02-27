7cdd6864cd1e612d4743fd574f9867c251e45cdd
xXRem08Xx <maissa.rem08@gmail.com>
2022-02-27 17:10:55 +0100

    ajout fichier

---
+++ header.php
@@ -0,0 +1,36 @@
+<head>
+
+  <!-- Besoins fondamentaux de la page
+  ================================================== -->
+  <meta charset="utf-8">
+
+  <!-- affichage dynamique du titre de la page -->
+  <title><?php echo $title; ?></title>
+
+  <!-- Métas spécifiques aux mobiles
+  ================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Creative Portfolio Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+  
+  <!-- ** Plugins nécessaires pour le site ** -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <!-- slick slider -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <!-- themefy-icon -->
+  <link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
+
+  <!-- Main Stylesheet -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <?php
+    include "bdd_connexion.php";
+  ?>
+
+
+
+</head>
