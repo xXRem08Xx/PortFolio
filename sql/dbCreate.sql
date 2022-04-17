a0e8764b9c423f6bfc13a7df775f1d7a73cea792
xXRem08Xx <maissa.rem08@gmail.com>
2022-04-17 15:46:39 +0200

    remplissage de la page stage-1.php

--- sql/dbCreate.sql
+++ sql/dbCreate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 14 avr. 2022 à 21:55
+-- Généré le : sam. 16 avr. 2022 à 21:35
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
   `numeroDocument` int(11) NOT NULL,
   PRIMARY KEY (`numeroCommentaire`),
   KEY `numeroDocument` (`numeroDocument`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `commentaire`
@@ -112,6 +112,7 @@ CREATE TABLE IF NOT EXISTS `document` (
   `lien` varchar(1000) DEFAULT NULL,
   `dateInsertion` date DEFAULT NULL,
   `description` varchar(2000) DEFAULT NULL,
+  `lienImage` varchar(200) NOT NULL,
   `lien_publication` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`numeroDocument`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
@@ -120,12 +121,12 @@ CREATE TABLE IF NOT EXISTS `document` (
 -- Déchargement des données de la table `document`
 --
 
-INSERT INTO `document` (`numeroDocument`, `titreProjet`, `lien`, `dateInsertion`, `description`, `lien_publication`) VALUES
-(1, 'Inscription des Producteurs', 'https://docs.google.com/document/d/1B2SWLEST26TSzouq9gkdH6OU3-tfrY2wssOfPZMiTM8/edit#', '2022-03-28', 'Ceci est la deuxième partie du projet NewWorld, où nous avons pour objectif de créer un formulaire d\'inscription,<br/>\r\nafin que les producteurs puisse d\'inscrire, en sécurisant au maximum les champs de saisie, et les valeurs rentrées. <br/> Des contraintes tel que l\'auto-complétion des code postal et des adresse via la base de données de l\'état ont été mise en place.', 'https://docs.google.com/document/d/e/2PACX-1vQOl4X8GPZ6W7aLWxVtC_BQMoJZXp6bhXRa65WKGjCYBd1f40M10ruqCVDDaBKgrNtRhCTcWlxwSWvy/pub?embedded=true'),
-(3, 'NewWorld Circuit Court', 'https://docs.google.com/document/d/1HorzfWplSx84AVCPDvZkDgu832t_Y6uvpQHbTd73blY/edit#heading=h.7oayvxboxfnc', '2022-03-28', 'C\'est la première activité du projet NewWorld,un projet de classe important.<br/>\r\nNous voulons créer une application capable de générer un PDF contenant tous les produits (fruits, légumes, viande, autres…) qui sont proposés par un producteur identifié par son id dans la base de données.', 'https://docs.google.com/document/d/e/2PACX-1vQHc6uRjDg9rKWmpQzVSFgw8CW3ZbbfjgCAKGPBdj4TUi_B697tRyCM-uUTLoflBA5-S3XG8vj6W-4O/pub?embedded=true'),
-(4, 'FireWall', 'https://docs.google.com/document/d/1H8N7gvsaANqJR9FY-wPZaLl3bwY9rk4HFDZL-9TSUFo/edit#heading=h.df45siibnle8', '2022-03-29', 'C\'est une application ayant pour rôle d\'administrer automatiquement les connexions externes.<br/>\r\nElle enregistre toute les IP tentant de se connecter et qui ont échoué, et si cette même IP échoue 3 fois ou plus, elle est bloqué.<br/>\r\nElle sera débloquée automatiquement si elle se connecte au minimum 5 minutes plus tard.<br/>\r\n\r\nUne interface graphique permet de suivre les IP échouant, et permet de les débloquer manuellement même si les 5 minutes ne sont pas passées.', 'https://docs.google.com/document/d/e/2PACX-1vR8X71vV1lo9la_JOqkhDIS0v94152AoffBhBx7Q93IJgeh_FzMQ_gsvmxlFUuJUDf4zZaBzG-kuxFt/pub?embedded=true'),
-(5, 'Évaluation De Multiplication', 'https://docs.google.com/document/d/1uVQMx03pf-cYBEtAMP3TrdqasaYP5wCeHdyPrsfhaNg/edit#heading=h.9fx6kvbuo4k7', '2022-03-29', 'C\'est un programme générant une interface graphique qui vous posera des questions sur les tables de multiplications.<br/>\r\n\r\nVous pouvez indiquer le nombre de questions que vous voulez, ainsi que la table que vous souhaitez faire en particulier si vous en avez envie.<br/>\r\n\r\nCe programme n\'est pas complet, car ne comporte pas de vérification de saisie dans les fenêtres de dialogues.', 'https://docs.google.com/document/d/e/2PACX-1vT2vhI7igRVrHi81FheVXAuk0ko_4k-oVzBl-NkVHRw7_L9fBCy_edzk_k-e73MVjBMhahQwZKVKX1c/pub?embedded=true'),
-(6, 'Évaluation Comparaison', 'https://docs.google.com/document/d/1IuluiB0Q6qpSE7kNh6C15mDW8qYzNLFY4ZBhDRNV5m4/edit', '2022-03-29', 'C\'est un programme générant une interface graphique soumettant 2 nombres, et l\'utilisateur doit choisir si le premier nombre est plus grand, égal ou plus petit que le second nombre.', 'https://docs.google.com/document/d/e/2PACX-1vRp6xCY6mi48CH1NnAEFB8eR4K4NP7LTS1aKPcuGdCYS4XrYSLd5VycXeKMLSiy7MTRxfF18wBLi4jm/pub?embedded=true');
+INSERT INTO `document` (`numeroDocument`, `titreProjet`, `lien`, `dateInsertion`, `description`, `lienImage`, `lien_publication`) VALUES
+(1, 'Inscription des Producteurs', 'https://docs.google.com/document/d/1B2SWLEST26TSzouq9gkdH6OU3-tfrY2wssOfPZMiTM8/edit#', '2022-03-28', 'Ceci est la deuxième partie du projet NewWorld, où nous avons pour objectif de créer un formulaire d\'inscription,<br/>\r\nafin que les producteurs puisse d\'inscrire, en sécurisant au maximum les champs de saisie, et les valeurs rentrées. <br/> Des contraintes tel que l\'auto-complétion des code postal et des adresse via la base de données de l\'état ont été mise en place.', 'inscription-producteur.png', 'https://docs.google.com/document/d/e/2PACX-1vQOl4X8GPZ6W7aLWxVtC_BQMoJZXp6bhXRa65WKGjCYBd1f40M10ruqCVDDaBKgrNtRhCTcWlxwSWvy/pub?embedded=true'),
+(3, 'NewWorld Circuit Court', 'https://docs.google.com/document/d/1HorzfWplSx84AVCPDvZkDgu832t_Y6uvpQHbTd73blY/edit#heading=h.7oayvxboxfnc', '2022-03-28', 'C\'est la première activité du projet NewWorld,un projet de classe important.<br/>\r\nNous voulons créer une application capable de générer un PDF contenant tous les produits (fruits, légumes, viande, autres…) qui sont proposés par un producteur identifié par son id dans la base de données.', 'circuit-court.png', 'https://docs.google.com/document/d/e/2PACX-1vQHc6uRjDg9rKWmpQzVSFgw8CW3ZbbfjgCAKGPBdj4TUi_B697tRyCM-uUTLoflBA5-S3XG8vj6W-4O/pub?embedded=true'),
+(4, 'FireWall', 'https://docs.google.com/document/d/1H8N7gvsaANqJR9FY-wPZaLl3bwY9rk4HFDZL-9TSUFo/edit#heading=h.df45siibnle8', '2022-03-29', 'C\'est une application ayant pour rôle d\'administrer automatiquement les connexions externes.<br/>\r\nElle enregistre toute les IP tentant de se connecter et qui ont échoué, et si cette même IP échoue 3 fois ou plus, elle est bloqué.<br/>\r\nElle sera débloquée automatiquement si elle se connecte au minimum 5 minutes plus tard.<br/>\r\n\r\nUne interface graphique permet de suivre les IP échouant, et permet de les débloquer manuellement même si les 5 minutes ne sont pas passées.', 'firewall.jpg', 'https://docs.google.com/document/d/e/2PACX-1vR8X71vV1lo9la_JOqkhDIS0v94152AoffBhBx7Q93IJgeh_FzMQ_gsvmxlFUuJUDf4zZaBzG-kuxFt/pub?embedded=true'),
+(5, 'Évaluation De Multiplication', 'https://docs.google.com/document/d/1uVQMx03pf-cYBEtAMP3TrdqasaYP5wCeHdyPrsfhaNg/edit#heading=h.9fx6kvbuo4k7', '2022-03-29', 'C\'est un programme générant une interface graphique qui vous posera des questions sur les tables de multiplications.<br/>\r\n\r\nVous pouvez indiquer le nombre de questions que vous voulez, ainsi que la table que vous souhaitez faire en particulier si vous en avez envie.<br/>\r\n\r\nCe programme n\'est pas complet, car ne comporte pas de vérification de saisie dans les fenêtres de dialogues.', 'table-multiplication.jpg', 'https://docs.google.com/document/d/e/2PACX-1vT2vhI7igRVrHi81FheVXAuk0ko_4k-oVzBl-NkVHRw7_L9fBCy_edzk_k-e73MVjBMhahQwZKVKX1c/pub?embedded=true'),
+(6, 'Évaluation Comparaison', 'https://docs.google.com/document/d/1IuluiB0Q6qpSE7kNh6C15mDW8qYzNLFY4ZBhDRNV5m4/edit', '2022-03-29', 'C\'est un programme générant une interface graphique soumettant 2 nombres, et l\'utilisateur doit choisir si le premier nombre est plus grand, égal ou plus petit que le second nombre.', 'comparaison.jpg', 'https://docs.google.com/document/d/e/2PACX-1vRp6xCY6mi48CH1NnAEFB8eR4K4NP7LTS1aKPcuGdCYS4XrYSLd5VycXeKMLSiy7MTRxfF18wBLi4jm/pub?embedded=true');
 
 -- --------------------------------------------------------
 
--- stage-1.php
+++ stage-1.php
@@ -38,7 +38,107 @@ require_once "./function.php";
     <section class="section">
         <div class="container">
             <div class="row">
+                <!DOCTYPE html>
+                <html lang="fr">
 
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+
+                <body>
+                    <!-- Section Presentation de l'entreprise -->
+                    <section>
+                        <h3 style="text-align: center;"><strong> Dignamik, Digne-les-bains</strong></h3>
+                        <h4><strong>Présentation de l'entreprise</strong></h4>
+                        <p>
+                            Dignamik est une petite start-up créée en juin 2019, se basant sur une activité de communication à travers
+                            les réseaux sociaux pour mettre en avant les commerçants locaux<br>
+                            souhaitant avoir une meilleure visibilité.
+                        </p>
+                        <p>
+                            Cette start-up est une pionnière de cette pratique en France, visant à augmenter la visibilité des commerçants locaux suivant votre localisation.<br>
+                            Pour cela, elle prend appui sur Facebook et Instagram principalement pour proposer du contenu personnalisé et unique pour chaque commerçant, vous permettant<br>
+                            de découvrir des nouveautés chaque jour !
+                        </p>
+                        <p>
+                            Vous pouvez retrouver leur projet <a href="https://www.viemaville.fr" target="_blank">ici</a> !
+                        </p>
+                        <div style="display: inline-block;">
+                            <p>
+                                Actuellement, ils sont présents dans 5 viles des Alpes-de-Haute-Provence :
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Digne-les-bains</li>
+                                <li class="list-group-item">Sisteron</li>
+                                <li class="list-group-item">Manosque</li>
+                                <li class="list-group-item">Oraison</li>
+                                <li class="list-group-item">Forcalquier</li>
+                            </ul>
+                        </div>
+                        <br>
+                        </p>
+                    </section>
+                    <section>
+                        <h4><strong>Déroulement du Stage</strong></h4>
+                        <p>
+                            Au cours de ce stage de 5 semaines, du 31 mai au 2 juillet, les objectifs de réalisations étaient :
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Réalisation d'une maquette du nouveau site de l'entreprise</li>
+                            <li class="list-group-item">Réalisation d'un cahier des charges</li>
+                            <li class="list-group-item">Réalisation du site internet sous wordpress, et gérer le responsiv</li>
+                        </ul>
+                        <br>
+                        </p>
+                    </section>
+                    <section>
+                        <h4><strong>Travail Effectué</strong></h4>
+                        <p>
+
+
+                        <h5>Réalisation de la maquette</h5>
+
+                        <p>
+                            Réaliser une maquette est essentiel pour bien saisir les envies du client, et en faire une ici était obligatoire.
+                            <br>
+                            Pour la création, j'ai utilisé un outil en ligne intitulé <a href="https://whimsical.com" target="_blank">whimsical</a> qui permet de schématiser des projets facilement et gratuitement.
+                            <br>
+                            Grâce à cet outil, j'ai pu réaliser ma maquette et la présenter à mon maître de stage et à mon patron, qui m'ont aidé pour définir les bases et les besoins que l'entreprise avaient.
+                        </p>
+                        <br>
+                        <h5>Réalisation du cahier des charges</h5>
+                        <p>
+                            Le cahier des charges est le point important de la création d'un site internet pour une personne tierce, car cela permet de bien lister toutes les obligations fixées par celle-ci et que l'on devra respecter dans le site internet.
+                            <br>
+                            Pour la création de ce cahier, j'ai utilisé <a href="https://www.canva.com" target="_blank">canva</a>, un outil en ligne permettant la réalisation de document facilement et facilement éditable / personifiable.
+                        </p>
+                        <br>
+                        <h5>Réalisation du site internet sous wordpress</h5>
+                        <p>
+                            Grâce à la maquette, réalise précédemment, la structure du site était déjà connue, et il ne restait alors qu'à utiliser les bons outils pour tous mettre en œuvre.
+                            <br>
+                            Pour cela, on utilise les extensions wordpress, gratuite ou payante, qui permettent d'élargir les possibilités de création.
+                            <br>
+                            Pour notre site, j'ai utilisé :
+                        <div style="display: inline-block;">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Elementor, pour gérer le contenu des pages et des effets de style qui les composera.</li>
+                                <li class="list-group-item">Sendinblue pour l'envoie des mails.</li>
+                                <li class="list-group-item">Envato éléments pour le template de base de wordpress, que l'on customise par la suite.</li>
+                                <li class="list-group-item">Yoast seo, pour gerer le referencement internet.</li>
+                                <li class="list-group-item">Akismet Anti-spam, pour empecher le floading des formulaires.</li>
+                                <li class="list-group-item">Simple Custom Css and Js, pour l'integration de code personnalisé, tel des widgets ou du Css supplementaire.</li>
+                                <li class="list-group-item">WpForms lite, pour la creation des formulaires.</li>
+                            </ul>
+                        </div>
+
+                        </p>
+                        </p>
+                    </section>
+                </body>
+
+                </html>
             </div>
         </div>
     </section>
