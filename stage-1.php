e3f0ab26322f5b1f5750e5ca12a7c4288b838a8b
xXRem08Xx <maissa.rem08@gmail.com>
2022-04-19 13:38:11 +0200

    finalisation  page stage-2.php + ajout details dans stage-1.php

--- stage-1.php
+++ stage-1.php
@@ -38,114 +38,103 @@ require_once "./function.php";
     <section class="section">
         <div class="container">
             <div class="row">
-                <!DOCTYPE html>
-                <html lang="fr">
 
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Document</title>
-                </head>
-
-                <body>
-                    <!-- Section Presentation de l'entreprise -->
-                    <section>
-                        <h3 style="text-align: center;"><strong> Dignamik, Digne-les-bains</strong></h3>
-                        <h4><strong>Présentation de l'entreprise</strong></h4>
+                <!-- Section Presentation de l'entreprise -->
+                <div class="container">
+                    <h3 style="text-align: center;"><strong> Dignamik, Digne-les-bains</strong></h3>
+                    <h4><strong>Présentation de l'entreprise</strong></h4>
+                    <p>
+                        Dignamik est une petite start-up créée en juin 2019, se basant sur une activité de communication à travers
+                        les réseaux sociaux pour mettre en avant les commerçants locaux<br>
+                        souhaitant avoir une meilleure visibilité.
+                    </p>
+                    <p>
+                        Cette start-up est une pionnière de cette pratique en France, visant à augmenter la visibilité des commerçants locaux suivant votre localisation.<br>
+                        Pour cela, elle prend appui sur Facebook et Instagram principalement pour proposer du contenu personnalisé et unique pour chaque commerçant, vous permettant<br>
+                        de découvrir des nouveautés chaque jour !
+                    </p>
+                    <p>
+                        Vous pouvez retrouver leur projet <a href="https://www.viemaville.fr" target="_blank">ici</a> !
+                    </p>
+                    <div style="display: inline-block;">
                         <p>
-                            Dignamik est une petite start-up créée en juin 2019, se basant sur une activité de communication à travers
-                            les réseaux sociaux pour mettre en avant les commerçants locaux<br>
-                            souhaitant avoir une meilleure visibilité.
-                        </p>
-                        <p>
-                            Cette start-up est une pionnière de cette pratique en France, visant à augmenter la visibilité des commerçants locaux suivant votre localisation.<br>
-                            Pour cela, elle prend appui sur Facebook et Instagram principalement pour proposer du contenu personnalisé et unique pour chaque commerçant, vous permettant<br>
-                            de découvrir des nouveautés chaque jour !
-                        </p>
-                        <p>
-                            Vous pouvez retrouver leur projet <a href="https://www.viemaville.fr" target="_blank">ici</a> !
-                        </p>
-                        <div style="display: inline-block;">
-                            <p>
-                                Actuellement, ils sont présents dans 5 viles des Alpes-de-Haute-Provence :
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Digne-les-bains</li>
-                                <li class="list-group-item">Sisteron</li>
-                                <li class="list-group-item">Manosque</li>
-                                <li class="list-group-item">Oraison</li>
-                                <li class="list-group-item">Forcalquier</li>
-                            </ul>
-                        </div>
-                        <br>
-                        </p>
-                    </section>
-                    <section>
-                        <h4><strong>Déroulement du Stage</strong></h4>
-                        <p>
-                            Au cours de ce stage de 5 semaines, du 31 mai au 2 juillet, les objectifs de réalisations étaient :
+                            Actuellement, ils sont présents dans 5 viles des Alpes-de-Haute-Provence :
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Réalisation d'une maquette du nouveau site de l'entreprise</li>
-                            <li class="list-group-item">Réalisation d'un cahier des charges</li>
-                            <li class="list-group-item">Réalisation du site internet sous wordpress, et gérer le responsiv</li>
+                            <li class="list-group-item">Digne-les-bains</li>
+                            <li class="list-group-item">Sisteron</li>
+                            <li class="list-group-item">Manosque</li>
+                            <li class="list-group-item">Oraison</li>
+                            <li class="list-group-item">Forcalquier</li>
                         </ul>
+                    </div>
+                    <br>
+                    </p>
+                </div>
+                <div class="container">
+                    <h4><strong>Déroulement du Stage</strong></h4>
+                    <p>
+                        Au cours de ce stage de 5 semaines, du 31 mai au 2 juillet, les objectifs de réalisations étaient :
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Réalisation d'une maquette du nouveau site de l'entreprise</li>
+                        <li class="list-group-item">Réalisation d'un cahier des charges</li>
+                        <li class="list-group-item">Réalisation du site internet sous wordpress, et gérer le responsiv</li>
+                    </ul>
+                    <br>
+                    </p>
+                </div>
+                <div class="container">
+                    <h4><strong>Travail Effectué</strong></h4>
+                    <p>
+
+                    <h5>Réalisation de la maquette</h5>
+
+                    <p>
+                        Réaliser une maquette est essentiel pour bien saisir les envies du client, et en faire une ici était obligatoire.
                         <br>
-                        </p>
-                    </section>
-                    <section>
-                        <h4><strong>Travail Effectué</strong></h4>
-                        <p>
-
-                        <h5>Réalisation de la maquette</h5>
-
-                        <p>
-                            Réaliser une maquette est essentiel pour bien saisir les envies du client, et en faire une ici était obligatoire.
-                            <br>
-                            Pour la création, j'ai utilisé un outil en ligne intitulé <a href="https://whimsical.com" target="_blank">whimsical</a> qui permet de schématiser des projets facilement et gratuitement.
-                            <br>
-                            Grâce à cet outil, j'ai pu réaliser ma maquette et la présenter à mon maître de stage et à mon patron, qui m'ont aidé pour définir les bases et les besoins que l'entreprise avaient.
-                        </p>
+                        Pour la création, j'ai utilisé un outil en ligne intitulé <a href="https://whimsical.com" target="_blank">whimsical</a> qui permet de schématiser des projets facilement et gratuitement.
                         <br>
-                        <h5>Réalisation du cahier des charges</h5>
-                        <p>
-                            Le cahier des charges est le point important de la création d'un site internet pour une personne tierce, car cela permet de bien lister toutes les obligations fixées par celle-ci et que l'on devra respecter dans le site internet.
-                            <br>
-                            Pour la création de ce cahier, j'ai utilisé <a href="https://www.canva.com" target="_blank">canva</a>, un outil en ligne permettant la réalisation de document facilement et facilement éditable / personifiable.
-                        </p>
+                        Grâce à cet outil, j'ai pu réaliser ma maquette et la présenter à mon maître de stage et à mon patron, qui m'ont aidé pour définir les bases et les besoins que l'entreprise avaient.
+                    </p>
+                    <br>
+                    <h5>Réalisation du cahier des charges</h5>
+                    <p>
+                        Le cahier des charges est le point important de la création d'un site internet pour une personne tierce, car cela permet de bien lister toutes les obligations fixées par celle-ci et que l'on devra respecter dans le site internet.
                         <br>
-                        <h5>Réalisation du site internet sous wordpress</h5>
-                        <p>
-                            Grâce à la maquette, réalise précédemment, la structure du site était déjà connue, et il ne restait alors qu'à utiliser les bons outils pour tous mettre en œuvre.
-                            <br>
-                            Pour cela, on utilise les extensions wordpress, gratuite ou payante, qui permettent d'élargir les possibilités de création.
-                            <br>
-                            Pour notre site, j'ai utilisé :
-                        <div style="display: inline-block;">
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Elementor, pour gérer le contenu des pages et des effets de style qui les composera.</li>
-                                <li class="list-group-item">Sendinblue pour l'envoie des mails.</li>
-                                <li class="list-group-item">Envato éléments pour le template de base de wordpress, que l'on customise par la suite.</li>
-                                <li class="list-group-item">Yoast seo, pour gerer le referencement internet.</li>
-                                <li class="list-group-item">Akismet Anti-spam, pour empecher le floading des formulaires.</li>
-                                <li class="list-group-item">Simple Custom Css and Js, pour l'integration de code personnalisé, tel des widgets ou du Css supplementaire.</li>
-                                <li class="list-group-item">WpForms lite, pour la creation des formulaires.</li>
-                            </ul>
-                        </div>
-                        </p>
+                        Pour la création de ce cahier, j'ai utilisé <a href="https://www.canva.com" target="_blank">canva</a>, un outil en ligne permettant la réalisation de document facilement et facilement éditable/personifiable.
+                    </p>
+                    <br>
+                    <h5>Réalisation du site internet sous wordpress</h5>
+                    <p>
+                        Grâce à la maquette, réalise précédemment, la structure du site était déjà connue, et il ne restait alors qu'à utiliser les bons outils pour tous mettre en œuvre.
+                        <br>
+                        Pour cela, on utilise les extensions wordpress, gratuite ou payante, qui permettent d'élargir les possibilités de création.
+                        <br>
+                        Pour notre site, j'ai utilisé :
+                    <div style="display: inline-block;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Elementor, pour gérer le contenu des pages et des effets de style qui les composera.</li>
+                            <li class="list-group-item">Sendinblue pour l'envoie des mails.</li>
+                            <li class="list-group-item">Envato éléments pour le template de base de wordpress, que l'on customise par la suite.</li>
+                            <li class="list-group-item">Yoast seo, pour gerer le referencement internet.</li>
+                            <li class="list-group-item">Akismet Anti-spam, pour empecher le floading des formulaires.</li>
+                            <li class="list-group-item">Simple Custom Css and Js, pour l'integration de code personnalisé, tel des widgets ou du Css supplementaire.</li>
+                            <li class="list-group-item">WpForms lite, pour la creation des formulaires.</li>
+                        </ul>
+                    </div>
+                    </p>
 
-                        <hr style="background-color: #41228e;">
 
-                        <p style="text-align: center;">
-                            Je tient a remercier Léo pour m'avoir offert la possibilité d'effectué mon stage dans son entreprise et de m'avoir fait confiance.😁😘
-                            <br>
-                            Je remercie egalement Jordanne pour m'avoir accompagné tous au long de ce stage, et de m'avoir aidé quand j'ai rencontré des difficultées. 💖
-                        </p>
-                    </section>
+                </div>
 
-                </body>
-
-                </html>
             </div>
+            <hr style="background-color: #41228e;">
+            <p style="text-align: center;">
+                Je tiens à remercier Léo pour m'avoir offert la possibilité d'effectuer mon stage dans son entreprise et de m'avoir fait confiance.😁😘
+                <br>
+                Je remercie également Jordane pour m'avoir accompagné tout au long de ce stage, et de m'avoir aidé quand j'ai rencontré des difficultés. 💖
+                <br>
+                Je remercie toute l'équipe qui m'a accueilli de la meilleure des manières 😁
+            </p>
         </div>
     </section>
 
--- stage-2.php
+++ stage-2.php
@@ -8,44 +8,177 @@ require_once "./function.php";
 ?>
 
 <body>
+	<?php
+	require_once "./nav-bar.php";
+	?>
+
+	<!-- titre de la page -->
+	<section class="page-title bg-primary position-relative">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-center">
+					<h1 class="text-white font-tertiary">Stage de 2ème année</h1>
+				</div>
+			</div>
+		</div>
+		<!-- image d'arriere plan -->
+		<img src="images/illustrations/page-title.png" alt="illustrations" class="bg-shape-1 w-100">
+		<img src="images/illustrations/leaf-pink-round.png" alt="illustrations" class="bg-shape-2">
+		<img src="images/illustrations/dots-cyan.png" alt="illustrations" class="bg-shape-3">
+		<img src="images/illustrations/leaf-orange.png" alt="illustrations" class="bg-shape-4">
+		<img src="images/illustrations/leaf-yellow.png" alt="illustrations" class="bg-shape-5">
+		<img src="images/illustrations/dots-group-cyan.png" alt="illustrations" class="bg-shape-6">
+		<img src="images/illustrations/leaf-cyan-lg.png" alt="illustrations" class="bg-shape-7">
+	</section>
+	<!-- /titre de la page -->
 
 
-    <?php
-    require_once "./nav-bar.php";
-    ?>
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<!-- presentation -->
+				<div class="container">
+					<h3 style="text-align: center;"><strong> Dignamik, Digne-les-bains</strong></h3>
+					<h4><strong>Présentation de l'entreprise</strong></h4>
+					<p>
+						Dignamik est une petite start-up créée en juin 2019, se basant sur une activité de communication à travers
+						les réseaux sociaux pour mettre en avant les commerçants locaux<br>
+						souhaitant avoir une meilleure visibilité.
+					</p>
+					<p>
+						Cette start-up est une pionnière de cette pratique en France, visant à augmenter la visibilité des commerçants locaux suivant votre localisation.<br>
+						Pour cela, elle prend appui sur Facebook et Instagram principalement pour proposer du contenu personnalisé et unique pour chaque commerçant, vous permettant<br>
+						de découvrir des nouveautés chaque jour !
+					</p>
+					<p>
+						Vous pouvez retrouver leur projet <a href="https://www.viemaville.fr" target="_blank">ici</a> !
+					</p>
+					<div style="display: inline-block;">
+						<p>
+							Actuellement, ils sont présents dans 11 viles des Alpes-de-Haute-Provence :
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Digne-les-bains</li>
+							<li class="list-group-item">Sisteron</li>
+							<li class="list-group-item">Manosque</li>
+							<li class="list-group-item">Oraison</li>
+							<li class="list-group-item">Moyenne Durance</li>
+							<li class="list-group-item">Vallée de la Blanche</li>
+							<li class="list-group-item">Vallée de l'Ubaye</li>
+							<li class="list-group-item">Gréoux-les-Bains</li>
+							<li class="list-group-item">Laragne-Montéglin</li>
+							<li class="list-group-item">Riez</li>
+							<li class="list-group-item">Forcalquier</li>
+						</ul>
+					</div>
+					<br>
+					</p>
+				</div>
 
-    <!-- titre de la page -->
-    <section class="page-title bg-primary position-relative">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="text-white font-tertiary">Stage de 2ème année</h1>
-                </div>
-            </div>
-        </div>
-        <!-- image d'arriere plan -->
-        <img src="images/illustrations/page-title.png" alt="illustrations" class="bg-shape-1 w-100">
-        <img src="images/illustrations/leaf-pink-round.png" alt="illustrations" class="bg-shape-2">
-        <img src="images/illustrations/dots-cyan.png" alt="illustrations" class="bg-shape-3">
-        <img src="images/illustrations/leaf-orange.png" alt="illustrations" class="bg-shape-4">
-        <img src="images/illustrations/leaf-yellow.png" alt="illustrations" class="bg-shape-5">
-        <img src="images/illustrations/dots-group-cyan.png" alt="illustrations" class="bg-shape-6">
-        <img src="images/illustrations/leaf-cyan-lg.png" alt="illustrations" class="bg-shape-7">
-    </section>
-    <!-- /titre de la page -->
+				<!-- objectif -->
+				<div class="container">
+					<br>
+					<h4><strong>Déroulement du Stage</strong></h4>
+					<p>
+						Au cours de ce stage de 5 semaines, du 31 mai au 2 juillet, les objectifs de réalisations étaient :
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item">Automatisation de la récupération et du traitement de données Facebook, Instagram et du site web pour être capable de les stocker</li>
+						<li class="list-group-item">Récolte et mise en forme des données afin d'en faciliter la lecture, la compréhension et l'utilisation</li>
+					</ul>
+					<br>
+					</p>
+				</div>
 
+				<!-- travail effectué -->
+				<div class="container">
+					<h4><strong>Travail Effectué</strong></h4>
+					<p>
+					<h5>Récupération des données</h5>
+					<p>
+						Cette partie est celle qui a nécessité le plus de temps d'investissement, notamment en termes de recherche et de lecture de documentation.
+						<br>
+						La première idée que l'on a mise en œuvre a été celle d'utiliser <a href="https://www.integromat.com/en">IntegroMath</a>, un service en ligne permettant de gérer et de programmer des taches,
+						grâce a l'utilisation d'API.
+						<br>
+						Par exemple, on peut programmer que lorsqu'on recoit un mail d'une certainne personne, ce mail est rangé dans un dossier de la boite mail et une notification nous est envoyée.
+						<br>
+						Nous avions pour idée de nous servir de ce service afin de programmer l'exécution mensuelle d'un call API a celle de Facebook et Instagram, afin de récupérer le contenu de ces calls et de les stocker.
+						<br>
+						Cependant, l'API Facebook est complexe, et j'ai trouvé dans la foulée un autre service web génial, nommé <a href="https://www.dataddo.com">Dataddo</a>, qui permet de récupérer les données depuis une ou plusieurs sources et de les transférer dans un fichier ou service externe, tel un fichier google sheet ou une base de données.
+						<br>
+						En profitant de cet outils, nous avons stocké toute les données de tous les comptes facebook et instagram depuis leurs creation, et nous les avons tranféré dans un Google Sheet créé pour l'occasion. (Le fichier fait alors plus de 10 000 lignes par onglet, sur plusieurs onglets)
+						<br>
+						<br>
+						Ce service étants payant à la fin de la période d'essai, nous avons opté pour un autre service web nommé <a href="https://www.zoho.com/fr/">Zoho</a> proposant une partie gratuite, et proposant une formule similaire, mais dans un fichier créé à chaque fois, obligeant l'action de copier le contenu du fichier et de le coller dans notre propre fichier adapté pour l'occasion.
+						<br>
 
-    <section class="section">
-        <div class="container">
-            <div class="row">
+					<h6><strong>Compte Rendu :</strong></h6>
+					Ce fût une partie longue, où le plus dur à été de trouver quel outil est le plus efficace et rentable, est-ce que c'est utilisé de payer pour ceci ou cela, etc ...
+					<br>
+					Elle m'a tout de même obligé à réfléchir à mes choix et à remettre en doute ce que j'ai effectué, afin de vraiment trouver la solution la plus utile et efficace.
+					</p>
+					</p>
 
-            </div>
-        </div>
-    </section>
+					<br>
+					<br>
+					<h5>Traitement des données</h5>
+					<p>
+						Cette partie a été celle qui m'a mis les nerfs à rude épreuve 😂
+						<br>
+						La cause étant que grâce au travail effectué en amont (expliqué ci-dessus), nous avions 3 onglets de 10 000 lignes d'informations.
+						<br>
+						Et ça requiert une connexion internet solide et inexistante pour moi.
+						<br>
+						Vient la partie où je dois organiser les données pour les rendre plus claires, et permettre de le faire automatiquement pour que les données rajoutées chaque mois soient prisent en compte. <br>
+					</p>
+					<ul class="list-group list-group-flush">
+						C'est pourquoi j'ai pensé à 2 solutions :
+						<li class="list-group-item">
+							Faire un graphique directement sur le fichier Google Sheet
+						</li>
+						<li class="list-group-item">
+							Utiliser Google studio pour faire un rapport
+						</li>
+					</ul>
+					<p>
+						La 2e solution m'a paru être la meilleure, et de loin : Google Studio permet de créer des rapports clairs, structurés et personnalisables, avec la possibilité d'y mettre des filtres suivant les informations qu'on lui donne. (date, nom d'entreprise, nombre de vue ...)
+						<br>
+						J'ai ainsi fait un rapport que j'ai présenté a léo, mon patron, et il a aimé l'idée et la présentation, mais
+						trouvait qu'on pouvait faire mieux. Il m'a alors demandé de trier les données dans le fichier Google Sheet contenant les données de base, et d'y séparer les informations pour pouvoir y mettre des filtres par la suite.
+						<br>
+						On a donc réorganise les données grace a des formules de somme et sélection de caractère, et pouvoir trier suivant le nom de l'entreprise concerné a été possible grâce à une longue formule. <br>
+						<br>
+						Cette fonction permet de récupérer la chaîne de caractère comprise entre 2 caractères indiqués (ici 🛍):
+					<div style="border: 1px solid silver;background-color: lightgray;display: inline-block;">
+						=SI(OU(C2="";STXT(C2;1;2)<>"🛍");"";STXT(SI(NBCAR(C2)-NBCAR(SUBSTITUE(C2;"🛍";""))<2;"";SUPPRESPACE(STXT(C2;TROUVE("🛍";C2)+1;TROUVE("🛍";C2;TROUVE("🛍";C2)+1)-TROUVE("🛍";C2)-1)));3;NBCAR(C2))) </div>
 
-    <?php
-    require_once "./footer.php";
-    ?>
+								</p>
+								<br>
+								<p>
+									Grâce à cette formule, on a pu récupérer le nom de l'entreprise de toutes les lignes récupère et la comparer avec la base de données de Dignamik afin de relier ces articles avec des noms, adresse mail, et numéro de téléphone.
+									<br>
+									Suite à cela, on a pu obtenir 2 jolis tableaux finals comptant le nombre de vue, de clic, et autre information pour chaque client, un pour le total des 30 derniers jours et un pour le total depuis leur inscription.
+									<br>
+									<br>
+									Il ne restait plus qu'à programmer l'envoi d'un mail préconçu aux clients dont on a l'adresse mail, avec leurs chiffres associés, pour faire un récapitulatif des 30 derniers jours.
+
+								</p>
+					</div>
+				</div>
+				<br>
+				<br>
+				<hr style="background-color: #41228e;">
+				<p style="text-align: center;">
+				Je tiens à remercier Léo pour m'avoir offert la possibilité d'effectuer mon stage dans son entreprise et de m'avoir fait confiance une deuxième fois, malgré ces temps difficiles.😁😘
+                <br>
+                Je remercie toute l'équipe qui m'a encore une fois accueilli de la meilleure des manières, dans une ambiance dynamique et joyeuse 😁
+            </p>
+
+			</div>
+	</section>
+
+	<?php
+	require_once "./footer.php";
+	?>
 
 
 </body>
